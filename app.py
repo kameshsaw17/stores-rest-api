4425377aa4580b0eec50fc54f5a4c7a7b13ca36d
Kamesh Saw <kameshbps@hotmail.com>
2021-01-05 13:31:03 +0530

    Fixed not importing db when running the app.

--- app.py
+++ app.py
@@ -12,11 +12,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.secret_key = 'kamesh'
 api=Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app ,authenticate, identity)  #/auth
+
 api.add_resource(Store,'/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>') 
 api.add_resource(ItemList, '/items')
